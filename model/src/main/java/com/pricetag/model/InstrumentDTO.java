9d868b4df4f60c9c49fbb00a3f230261edd599f1
TOMPAY\dp0156 <taner.cakiroglu@tompay.com.tr>
2022-07-28 20:43:35 +0300

    get synced

--- model/src/main/java/com/pricetag/model/InstrumentDTO.java
+++ model/src/main/java/com/pricetag/model/InstrumentDTO.java
@@ -6,6 +6,8 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.ToString;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 import javax.xml.bind.annotation.XmlAccessType;
 import javax.xml.bind.annotation.XmlAccessorType;
 import javax.xml.bind.annotation.XmlElement;
@@ -29,10 +31,13 @@ public class InstrumentDTO implements Serializable {
 
     private static final long serialVersionUID = 4294828978771772354L;
     @XmlElement
+    @NotBlank(message = "name.invalid")
     private String name;
     @XmlElement
+    @NotNull(message = "price.invalid")
     private BigDecimal price;
     @XmlElement
+    @NotNull(message = "vendor.invalid")
     private String vendor;
     @XmlTransient
     private Long createdDate;
